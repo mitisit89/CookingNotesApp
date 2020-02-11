902787b736eb211e677e3ec3c8fe4d533955bb76
Wladislaw <mitisit89@gmail.com>
2020-02-11 20:38:34 +0300

    формы регистрации и логина

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Users(db.Model):
@@ -7,6 +8,22 @@ class Users(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Posts', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User s%>' % self.username
+        return '<User %s>' % self.username
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    slug = db.Column(db.String(140))
+    body = db.Column(db.Text)
+    time_stamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
--- view.py
+++ view.py
@@ -1,7 +1,29 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for
+from app import app, db
+from form import LoginForm, RegistrationForm, Users
+from flask_login import current_user
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('login_form.html', form=form)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = Users(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('registration_form.html', title='Register', form=form)
