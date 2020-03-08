8200a95491f47657366dd646906453a29ff92b91
Wladislaw <mitisit89@gmail.com>
2020-03-08 18:47:36 +0300

    добавлена возможность редактирования записей

--- post/__init__.py
+++ post/__init__.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
-from forms import CreatePostForm
+from forms import PostForm
 from app.models import Posts, db
 import re
 from time import time
+
 post = Blueprint('post', __name__, template_folder='templates')
 
 
@@ -21,7 +22,7 @@ def index(slug):
 @login_required
 @post.route('/create', methods=['GET', 'POST'])
 def create():
-    form = CreatePostForm()
+    form = PostForm()
     if form.validate_on_submit():
         new_post = Posts(title=form.title.data, body=form.body.data, slug=slugify(form.title.data), author=current_user)
         db.session.add(new_post)
@@ -30,6 +31,15 @@ def create():
     return render_template('create_post.html', form=form)
 
 
-@post.route('/edit/<str>')
-def edit():
-    pass
+@login_required
+@post.route('/<slug>/edit/', methods=['GET', 'POST'])
+def edit(slug):
+    post_for_edit = Posts.query.filter(Posts.slug == slug).first()
+    if request.method == 'POST':
+        edited_post = PostForm(formdata=request.form, obj=post_for_edit)
+        edited_post.populate_obj(post_for_edit)
+        db.session.commit()
+        return redirect(url_for('post.index', slug=post_for_edit.slug))
+    else:
+        form = PostForm(obj=post_for_edit)
+        return render_template('edit_post.html', form=form, post=post_for_edit)
