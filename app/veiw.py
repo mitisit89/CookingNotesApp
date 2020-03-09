23851be9795ed6b9d49831d03a22f2474c94e4a5
Wladislaw <mitisit89@gmail.com>
2020-03-09 14:39:51 +0300

    Добавлен поиск.

--- app/veiw.py
+++ app/veiw.py
@@ -1,13 +1,13 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.models import Posts
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    posts = Posts.query.all()
+    q = request.args.get('q')
+    if q:
+        posts = Posts.query.filter(Posts.title.contains(q) | Posts.body.contains(q)).all()
+    else:
+        posts = Posts.query.all()
     return render_template('index.html', posts=posts)
-
-
-def search():
-    pass
